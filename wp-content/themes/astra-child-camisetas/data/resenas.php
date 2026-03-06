28b3c53d325e689a255944f38504997194e902c0
danipereiracodes <info.danipereira@gmail.com>
2026-03-06 10:43:35 +0100

    style: enhance layout and styles for category cards and testimonials on home template

---
+++ wp-content/themes/astra-child-camisetas/data/resenas.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Archivo de reseñas de Google
+ * Para añadir o eliminar reseñas, edita este array.
+ * Campos: nombre, tiempo, texto
+ */
+
+$resenas = [
+    [
+        'nombre' => 'moadyd',
+        'tiempo' => 'Hace un año',
+        'texto'  => 'Todo genial, hicimos un monton de ropa y los colores, el trato, el precio y el tiempo me parece todo muy correcto, falta ver como durara cn el tiempo el logo pero la verdad que asi de primeras lo recomiendo sin duda ✌️',
+    ],
+    [
+        'nombre' => 'Maria Salazar',
+        'tiempo' => 'Hace 11 meses',
+        'texto'  => 'Un 10!! Trato increíble, la dependienta es un encanto y me hizo el trabajo en menos de lo esperado.',
+    ],
+    [
+        'nombre' => 'Teresa Briones',
+        'tiempo' => 'Hace 8 meses',
+        'texto'  => 'Todo perfecto, me atendió una chica que se llama Irene que es encantadora y muy eficiente.',
+    ],
+    [
+        'nombre' => 'Érika DuBaele',
+        'tiempo' => 'Hace 2 años',
+        'texto'  => 'Muy recomendado. La calidad de la impresión es genial y los precios estan super bien. A destacar el trato de l@s trabajador@s, muy cercano y atento.',
+    ],
+    [
+        'nombre' => 'Fermín Roldán',
+        'tiempo' => 'Hace 3 años',
+        'texto'  => 'Irene y Jose siempre tan profesionales. Siempre atentos!! Me facilitan las camisetas que más se adaptan a mi negocio y las impresiones.',
+    ],
+    [
+        'nombre' => 'Raúl Ramperez',
+        'tiempo' => 'Hace 4 años',
+        'texto'  => 'Maravilloso!!! Les he hecho un pedido desde Zaragoza y un lujo: rápidos, buenas calidades y buen precio sólo mejorado por el gran trato que dan. Sin ninguna duda repetiré.',
+    ],
+    [
+        'nombre' => 'Carlos Bermudez',
+        'tiempo' => 'Hace 8 meses',
+        'texto'  => 'Rápido, barato y buena calidad. Muy recomendable.',
+    ],
+    [
+        'nombre' => 'Nerea Zabalo',
+        'tiempo' => 'Hace un año',
+        'texto'  => 'Muy buena atención, servicio excelente, muy buena calidad y gran rapidez en el pedido. Sin duda la mejor opción de impresión en Logroño.',
+    ],
+    [
+        'nombre' => 'Maria Ibañez',
+        'tiempo' => 'Hace 6 años',
+        'texto'  => 'Excelente orientación en diseño, con una atención amable al cliente. Muy profesionales tanto en la calidad del trabajo terminado, como en el plazo de entrega previsto, y además a un precio razonable.',
+    ],
+    [
+        'nombre' => 'SARA MORLOR',
+        'tiempo' => 'Hace 6 años',
+        'texto'  => 'Una muy buena atención y asesoramiento. Orientación de diseño y calidades... El resultado sido muy bueno y todo dentro de plazo a pesar del envío. Muchas gracias por todo chicos! ;)',
+    ],
+    [
+        'nombre' => 'Cristina G',
+        'tiempo' => 'Hace 4 años',
+        'texto'  => 'Me mandaron la camiseta un viernes y el lunes ya estaba en Madrid! Muy maja la chica con la que hablé por email, me atendieron súper rápido y fueron muy amables. Seguiré pidiendo mis camisetas aquí tanto por la calidad como por el trato y el precio.',
+    ],
+    [
+        'nombre' => 'Rocio Corres Toyas',
+        'tiempo' => 'Hace 5 años',
+        'texto'  => 'Profesionalidad y trato excelente. Encargué unas mochilas para el colegio y el resultado fue brillante. Los niños quedaron encantados. ¡Repetiré sin duda!',
+    ],
+];
